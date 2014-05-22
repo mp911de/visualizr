96b3d17ba911c387d07f5d34b0ea5fb616f4d4d7
Mark Paluch <mpaluch@paluch.biz>
2014-05-22 15:21:11 +0200

    Create ChartDataSource.java

---
+++ model/src/main/java/biz/paluch/visualizr/model/ChartDataSource.java
@@ -0,0 +1,37 @@
+package biz.paluch.visualizr.model;
+
+import java.io.Serializable;
+
+/**
+ * @author <a href="mailto:mpaluch@paluch.biz">Mark Paluch</a>
+ * @since 13.04.14 11:15
+ */
+public class ChartDataSource implements Serializable {
+    private String id;
+
+    private String name;
+
+    public ChartDataSource() {
+    }
+
+    public ChartDataSource(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
